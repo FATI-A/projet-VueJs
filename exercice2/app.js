c4b788606ad9980db304e2dc5a94795b803b4159
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-07-15 15:42:16 +0200

    update

--- exercice2/app.js
+++ exercice2/app.js
@@ -4,6 +4,20 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
-});
-
+  methods: {
+    addCounter(num) {
+      // return this.counter=+num; ajouter une seule fois 
+       return this.counter = this.counter + num
+    },
+    // addCounter(num) {
+    //   return this.counter++;
+    // },
+    removeCounter(num) {
+      // return this.counter =-10;
+      
+       return this.counter = this.counter - num
+    },
+    },
+  },
+);
 app.mount('#events');
