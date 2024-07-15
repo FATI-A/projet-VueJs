a01502f921d89e109cb294f8d294b710909b884f
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-07-15 16:39:44 +0200

    update

--- exercice2/app.js
+++ exercice2/app.js
@@ -1,23 +1,47 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 12,
+      name: "",
+      confirmedName:" ",
     };
   },
   methods: {
     addCounter(num) {
-      // return this.counter=+num; ajouter une seule fois 
-       return this.counter = this.counter + num
+      // return this.counter=+num; ajouter une seule fois
+      return (this.counter = this.counter + num);
     },
     // addCounter(num) {
     //   return this.counter++;
     // },
     removeCounter(num) {
       // return this.counter =-10;
-      
-       return this.counter = this.counter - num
+
+      return (this.counter = this.counter - num);
     },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
     },
+    // setName(event) {
+    //   this.name = event.target.value ;
+    // },
+    // setName() {
+    //   return this.name
+    // },
+    // submitForm(event) {
+    //   event.preventDefault();
+    //   // l'action par défaut lorsque vous cliquez sur ce bouton est de soumettre le formulaire et de rafraîchir la page. En utilisant event.preventDefault(), empêcher ce comportement par défaut.
+    //   alert("submitted !");
+    // },
+    submitForm() {
+       alert("submitted !");
+     },
+     confimInput(){
+      this.confirmedName =this.name;
+     },
+     resetInput(){
+      this.name=''
+     }
   },
-);
-app.mount('#events');
+});
+app.mount("#events");
