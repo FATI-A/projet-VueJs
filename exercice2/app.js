246dc85b470805378aca7b6793f17b267ceae0a0
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-07-15 17:52:41 +0200

    update

--- exercice2/app.js
+++ exercice2/app.js
@@ -6,55 +6,79 @@ const app = Vue.createApp({
       confirmedName: " ",
       Lastname: " ",
       last: " ",
+      fullname: "",
     };
   },
-  methods: {
-    addCounter(num) {
-      // return this.counter=+num; ajouter une seule fois
-      return (this.counter = this.counter + num);
-    },
-    // addCounter(num) {
-    //   return this.counter++;
-    // },
-    removeCounter(num) {
-      // return this.counter =-10;
-
-      return (this.counter = this.counter - num);
-    },
-    // setName(event, lastName) {
-    //   this.name = event.target.value + " " + lastName;
-    // },
-    // setName(event) {
-    //   this.name = event.target.value ;
-    // },
-    // setName() {
-    //   return this.name
-    // },
-    // submitForm(event) {
-    //   event.preventDefault();
-    //   // l'action par défaut lorsque vous cliquez sur ce bouton est de soumettre le formulaire et de rafraîchir la page. En utilisant event.preventDefault(), empêcher ce comportement par défaut.
-    //   alert("submitted !");
-    // },
-    outPutFullName() {
-      if (this.name === "") {
-        return " ";
+  watch: {
+    Lastname(value) {
+      if (value === "") {
+        this.fullname = "";
+      } else {
+        this.fullname = this.name + " " + value;
       }
-      return this.name + " " + this.Lastname;
     },
-    submitForm() {
-      alert("submitted !");
-    },
-    confimInput() {
-      this.confirmedName = this.name;
-    },
-    lastLast() {
-      this.last = this.Lastname;
-    },
-    resetInput() {
-      this.name = "";
-      this.Lastname=" ",
-      this.last="",
-      this.confirmedName= " "
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + " " + this.Lastname;
+    //   }
+    // },
+  },
+  computed: {
+    //   fullName() {
+    //     if (this.name === "" || this.last === "") {
+    //       return " ";
+    //     }
+    //     return this.name + " " + this.Lastname;
+    //   },
+    // },
+    methods: {
+      addCounter(num) {
+        // return this.counter=+num; ajouter une seule fois
+        return (this.counter = this.counter + num);
+      },
+      // addCounter(num) {
+      //   return this.counter++;
+      // },
+      removeCounter(num) {
+        // return this.counter =-10;
+
+        return (this.counter = this.counter - num);
+      },
+      // setName(event, lastName) {
+      //   this.name = event.target.value + " " + lastName;
+      // },
+      // setName(event) {
+      //   this.name = event.target.value ;
+      // },
+      // setName() {
+      //   return this.name
+      // },
+      // submitForm(event) {
+      //   event.preventDefault();
+      //   // l'action par défaut lorsque vous cliquez sur ce bouton est de soumettre le formulaire et de rafraîchir la page. En utilisant event.preventDefault(), empêcher ce comportement par défaut.
+      //   alert("submitted !");
+      // // },
+      // outPutFullName() {
+      //   if (this.name === "") {
+      //     return " ";
+      //   }
+      //   return this.name + " " + this.Lastname;
+      // },
+      submitForm() {
+        alert("submitted !");
+      },
+      confimInput() {
+        this.confirmedName = this.name;
+      },
+      lastLast() {
+        this.last = this.Lastname;
+      },
+      resetInput() {
+        this.name = "";
+        (this.Lastname = " "), (this.last = ""), (this.confirmedName = " ");
+      },
     },
   },
 });
