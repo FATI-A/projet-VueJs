d5220a6df547e402603d338eda96867a1508bc79
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-08-06 10:56:12 +0200

    update

--- inverse.js
+++ inverse.js
@@ -4,11 +4,11 @@ function reverseString(str) {
 
 console.log(reverseString("hello")); // Output: 'olleh'
 
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
+// function removeDuplicates(arr) {
+//   return [...new Set(arr)];
+// }
 
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4,4, 5])); // Output: [1, 2, 3, 4, 5]
+// console.log(removeDuplicates([1, 2, 2, 3, 4, 4,4, 5])); // Output: [1, 2, 3, 4, 5]
 
 const person = {
   name: "Alice",
