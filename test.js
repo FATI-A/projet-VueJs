d6fa5b13da7ddcb72cb07a298b5ca6e59dfe1fa5
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-08-14 10:37:17 +0200

    update

--- test.js
+++ test.js
@@ -57,3 +57,24 @@ function removeDuplicates(arr) {
 
 // Test
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
+
+function areObjectsEqual(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Test
+console.log(areObjectsEqual({a: 1, b: 2}, {a: 1, b: 2})); // Output: true
+console.log(areObjectsEqual({a: 1, b: 2}, {a: 1, b: 3})); // Output: false
