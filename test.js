22f21338199aff487e540d996028229f73b74c31
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-08-26 11:36:16 +0200

    update

--- test.js
+++ test.js
@@ -96,3 +96,25 @@ function debounce(func, wait) {
   };
 }
 
+function areObjectsEqual(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Test
+console.log(areObjectsEqual({a: 1, b: 2}, {a: 1, b: 2})); // Output: true
+console.log(areObjectsEqual({a: 1, b: 2}, {a: 1, b: 3})); // Output: false
+
+
