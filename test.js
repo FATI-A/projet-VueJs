ad7b02e305ba102219c4558ea885b2d36db32c6a
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-08-28 11:15:19 +0200

    update

--- test.js
+++ test.js
@@ -163,3 +163,18 @@ function countVowels(str) {
 console.log(countVowels('hello')); // Output: 2
 console.log(countVowels('programming')); // Output: 3
 
+function mergeSortedArrays(arr1, arr2) {
+  let merged = [], i = 0, j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
+    } else {
+      merged.push(arr2[j]);
+      j++;
+    }
+  }
+  return merged.concat(arr1.slice(i)).concat(arr2.slice(j));
+}
+
+console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
