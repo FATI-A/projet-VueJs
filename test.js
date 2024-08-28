708c792294af4ad577797bce58d7038e59ee8216
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-08-28 11:16:22 +0200

    update

--- test.js
+++ test.js
@@ -178,3 +178,13 @@ function mergeSortedArrays(arr1, arr2) {
 }
 
 console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
+
+
+const obj = {
+  method: function () {
+    console.log(this);
+  },
+};
+
+const method = obj.method;
+method();
