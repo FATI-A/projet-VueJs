0d2bce409a62ab808d9781e6cd64a2e50343ce51
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-08-29 10:55:28 +0200

    update

--- test.js
+++ test.js
@@ -148,6 +148,12 @@ function filterByAge(array, age) {
 
 console.log(filterByAge(data, 25)); // Output: [{ name: 'Bob', age: 30 }]
 
+function countOccurrences(arr, value) {
+  return arr.reduce((count, item) => item === value ? count + 1 : count, 0);
+}
+
+console.log(countOccurrences([1, 2, 3, 2, 2, 3, 4], 2)); // Output: 3
+
 
 // function countVowels(str) {
 //   const vowels = 'aeiouAEIOU';
