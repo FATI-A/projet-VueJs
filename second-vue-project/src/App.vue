162ad43befaef6e4d426ec2356baa41427e9e894
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-09-27 11:34:24 +0200

    update

---
+++ second-vue-project/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <friend-contact
+      v-for="friend in friends"
+      :key="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="true"
+      ></friend-contact>
+          <!-- ici on peut pas utiliser PhoneNumber il faut mettre - pour le format html -->
+      <!-- <friend-contact
+        name="jolie jones"
+        phone-number="0612354895"
+        email-address="julie@localhost.com"
+         is-favorite= "0"
+      ></friend-contact>
+      <friend-contact
+        name="lola jones"
+        phone-number="0612354895"
+        email-address="lola@localhost.com"
+         is-favorite= "0"
+      ></friend-contact> -->
+    </ul>
+  </section>
+</template>
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: " manuel",
+          name: "Manuel lorenez",
+          phone: " 0612354897",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie ",
+          name: "jolie jones",
+          phone: "0612354895",
+          email: "julie@localhost.com",
+        },
+        {
+          id: "lola ",
+          name: "lola jones",
+          phone: " 0612354895",
+          email: "lola@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ second-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,73 @@
+<template>
+  <li>
+    <h2>{{ name }} {{friendsIsFavorite  === true ? '(favorite )' : ''}}</h2>
+    <button @click="toggleDetails">
+      {{ detailsVisible ? "hide" : "show" }} Details
+    </button>
+     <button @click="toggleFavorites">
+       Favorite
+    </button>
+    <ul v-if="detailsVisible">
+      <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  // props:[
+  //     'name',
+  //     'phoneNumber',
+  //     'emailAddress',
+//   'isFavorite',
+  // ],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default : false,
+    //   validator : function (value ){
+    //     return value ==="1" || value ==="0"
+    //   }
+    },
+  },
+  data() {
+    return {
+      detailsVisible: false,
+      //   friend: {
+      //     id: " manuel",
+      //     name: "Manuel lorenez",
+      //     phone: " 0612354897",
+      //     email: "manuel@localhost.com",
+      //   },
+      friendsIsFavorite : this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsVisible = !this.detailsVisible;
+    },
+      toggleFavorites() {
+    if(this.friendsIsFavorite === true)
+  { 
+    this.friendsIsFavorite = false
+  } else {
+    this.friendsIsFavorite = true
+  }
+  }
+  },
+
+};
+</script>
